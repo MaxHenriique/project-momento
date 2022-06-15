ac6becb3a3e01c9478716775b22720056cbf4d69
= <=>
2022-06-15 12:13:15 -0300

    project update

--- src/components/equipeComponents/slideVideos/Apresentacao.jsx
+++ src/components/equipeComponents/slideVideos/Apresentacao.jsx
@@ -7,9 +7,7 @@ function Apresentacao({hard, nome, funcao, soft, textOne, textTwo, avatar, movie
                 <h2 className='title'>{hard}</h2>
 
                 <div className='video'>
-                    <video width="950" height="450" controls>
-                        <source src={movie} type="video/mp4"  width="950"/>
-                    </video>
+                    {movie}
                     
                     <div className='person'>
                         <div className='avatarPerson'>
